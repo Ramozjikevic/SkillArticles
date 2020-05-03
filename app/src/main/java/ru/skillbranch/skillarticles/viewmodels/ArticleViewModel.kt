407c57290dafaf1069c2c7ad32cd4c5972c8f687
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-05-03 18:32:18 +0500

    Error correction in ArticleViewModel

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -8,9 +8,8 @@ import ru.skillbranch.skillarticles.extensions.data.toAppSettings
 import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
 import ru.skillbranch.skillarticles.extensions.format
 
-class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleState>(ArticleState()) {
+class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleState>(ArticleState()), IArticleViewModel {
     private val repository = ArticleRepository
-
 
     init {
         subscribeOnDataSource(getArticleData()){ article, state ->
@@ -18,6 +17,7 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
             state.copy(
                 shareLink = article.shareLink,
                 title = article.title,
+                author = article.author,
                 category = article.category,
                 categoryIcon = article.categoryIcon,
                 date = article.date.format()
@@ -49,44 +49,46 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
     }
 
     //load text from network
-    private fun getArticleContent() : LiveData<List<Any>?> {
+    override fun getArticleContent() : LiveData<List<Any>?> {
         return repository.loadArticleContent(articleId)
     }
 
     //load data from db
-    private fun getArticleData() : LiveData<ArticleData?> {
+    override fun getArticleData() : LiveData<ArticleData?> {
         return repository.getArticle(articleId)
     }
 
     //load data from db
-    private fun getArticlePersonalInfo() : LiveData<ArticlePersonalInfo?> {
+    override fun getArticlePersonalInfo() : LiveData<ArticlePersonalInfo?> {
         return repository.loadArticlePersonalInfo(articleId)
     }
 
-    fun handleNightMode() {
+    override fun handleNightMode() {
         val settings = currentState.toAppSettings()
         repository.updateSettings(settings.copy(isDarkMode = !settings.isDarkMode))
     }
 
-    fun handleUpText() {
+    override fun handleUpText() {
         repository.updateSettings(currentState.toAppSettings().copy(isBigText = true))
     }
 
-    fun handleDownText() {
+    override fun handleDownText() {
         repository.updateSettings(currentState.toAppSettings().copy(isBigText = false))
     }
 
-    fun handleLike() {
+    override fun handleLike() {
+
+        val isLike = currentState.isLike
         val toggleLike = {
             val info = currentState.toArticlePersonalInfo()
             repository.updateArticlePersonalInfo(info.copy(isLike = !info.isLike))
         }
 
         toggleLike()
-        val msg = if (currentState.isLike) Notify.TextMessage("Mark is liked")
+        val msg = if (!isLike) Notify.TextMessage("Mark is liked")
         else{
             Notify.ActionMessage(
-                "Don't like it anymore",
+                "Don`t like it anymore",
                 "No, still like it",
                 toggleLike
             )
@@ -94,33 +96,46 @@ class ArticleViewModel(private val articleId: String) : BaseViewModel<ArticleSta
         notify(msg)
     }
 
-    fun handleBookmark() {
+    override fun handleBookmark() {
+        val info = currentState.toArticlePersonalInfo()
+        repository.updateArticlePersonalInfo(info.copy(isBookmark = !info.isBookmark))
+
+        val msg = if (currentState.isBookmark) "Add to bookmarks" else "Remove from bookmarks"
+        notify(Notify.TextMessage(msg))
     }
 
-    fun handleShare() {
-        val msg = " Share is not implemented"
-        notify(Notify.ErrorMessage(msg, "ОК", null))
+    override fun handleShare() {
+        val msg = "Share is not implemented"
+        notify(Notify.ErrorMessage(msg, "OK", null))
     }
 
-    fun handleToggleMenu() {
+    override fun handleToggleMenu() {
         updateState { it.copy(isShowMenu = !it.isShowMenu) }
+    }
+
+    override fun handleSearchMode(isSearch: Boolean) {
+
+    }
+
+    override fun handleSearch(query: String?) {
+
     }
 
 }
 
 data class ArticleState(
     val isAuth: Boolean = false, //пользователь авторизован
-    val isLoadingContent: Boolean = true, //контент загружен
+    val isLoadingContent: Boolean = true, //контент загружается
     val isLoadingReviews: Boolean = true, //отзывы загружаются
-    val isLike: Boolean = false, // отмечено как Like
-    val isBookmark: Boolean = false, // в закладках
-    val isShowMenu: Boolean = false, // отображаеться меню
-    val isBigText: Boolean = false, // шрифт увеличен
-    val isDarkMode: Boolean = false, // темный режим
+    val isLike: Boolean = false, //отмечено как Like
+    val isBookmark: Boolean = false, //в закладках
+    val isShowMenu: Boolean = false, //отображается меню
+    val isBigText: Boolean = false, //шрифт увеличен
+    val isDarkMode: Boolean = false, //темный режим
     val isSearch: Boolean = false, // режим поиска
-    val searchQuery: String? = null, // поисковой запрос
-    val searchResults: List<Pair<Int, Int>> = emptyList(), // результат поиска(стартовая и конечная позиции)
-    val searchPosition: Int = 0, // текущая позиция найденного результата,
+    val searchQuery: String? = null, // поисковый запрос
+    val searchResults: List<Pair<Int, Int>> = emptyList(), // результаты поиска (стартовая и конечная позиция)
+    val searchPosition: Int = 0, // текущая позиция найденного результата
     val shareLink: String? = null, // ссылка Share
     val title: String? = null, // заголовок статьи
     val category: String? = null, // категория
@@ -129,5 +144,5 @@ data class ArticleState(
     val author: Any? = null, // автор статьи
     val poster: String? = null, // обложка статьи
     val content: List<Any> = emptyList(), // контент
-    val reviews: List<Any> = emptyList() // комментарий
+    val reviews: List<Any> = emptyList() // комментарии
 )
