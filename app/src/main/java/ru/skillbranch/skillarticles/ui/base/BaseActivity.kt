73fd980c2d7fea63ebbd3881d6d3dfff0a08bb9b
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-05-14 19:38:58 +0500

    ViewModelProvideDone

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.skillarticles.ui.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import ru.skillbranch.skillarticles.viewmodels.ViewModelDelegate
 import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 import ru.skillbranch.skillarticles.viewmodels.base.Notify
@@ -34,5 +36,9 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
         super.onRestoreInstanceState(savedInstanceState)
         viewModel.restoreState(savedInstanceState)
         binding.restoreUi(savedInstanceState)
+    }
+
+    internal inline fun <reified T : ViewModel> provideViewModel(arg: Any?): ViewModelDelegate<T> {
+        return ViewModelDelegate(T::class.java, arg)
     }
 }
---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ViewModelDelegate.kt
@@ -0,0 +1,40 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import ru.skillbranch.skillarticles.viewmodels.base.ViewModelFactory
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/*
+class AttrValue(@AttrRes private val res: Int) : ReadOnlyProperty<Context, Int> {
+    private var value: Int? = null
+    override fun getValue(thisRef: Context, property: KProperty<*>): Int {
+        if (value == null) {
+            val tv = TypedValue()
+            if (thisRef.theme.resolveAttribute(res, tv, true)) value = tv.data
+            else throw  Resources.NotFoundException("Resource with id $res not found")
+        }
+        return value!!
+    }
+}*/
+
+/*    override val viewModel: ArticleViewModel by lazy {
+        val vmFactory = ViewModelFactory("0")
+        ViewModelProviders.of(this, vmFactory).get(ArticleViewModel::class.java)
+    }*/
+
+class ViewModelDelegate<T: ViewModel>(private val clazz: Class<T>, private val arg: Any?) : ReadOnlyProperty<FragmentActivity, T> {
+
+    private lateinit var value: T
+
+    override fun getValue(thisRef: FragmentActivity, property: KProperty<*>): T {
+        if (!::value.isInitialized) value = when (arg) {
+            null -> ViewModelProviders.of(thisRef).get(clazz)
+            else -> ViewModelProviders.of(thisRef, ViewModelFactory(arg)).get(clazz)
+        }
+        return value
+    }
+
+}
