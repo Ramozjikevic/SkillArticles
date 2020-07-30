a6e79a93c93430f5336c7b6da25fe6d0c9084091
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-07-31 03:34:34 +0500

    create default method

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/IMarkdownView.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/markdown/IMarkdownView.kt
@@ -1,6 +1,10 @@
 package ru.skillbranch.skillarticles.ui.custom.markdown
 
 import android.text.Spannable
+import android.text.SpannableString
+import androidx.core.text.getSpans
+import ru.skillbranch.skillarticles.ui.custom.spans.SearchFocusSpan
+import ru.skillbranch.skillarticles.ui.custom.spans.SearchSpan
 
 
 interface IMarkdownView {
@@ -11,17 +15,38 @@ interface IMarkdownView {
         results: List<Pair<Int, Int>>,
         offset: Int
     ) {
-        //TODO implement me
+        clearSearchResult()
+        val offsetResult = results
+            .map { (start, end) -> start.minus(offset) to end.minus(offset)}
+
+        try {
+        offsetResult.forEach { (start, end) ->
+            spannableContent.setSpan(
+                SearchSpan(),
+                start,
+                end,
+                SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+        } catch (e: Exception) {
+
+        }
     }
 
     fun renderSearchPosition(
         searchPosition: Pair<Int, Int>,
         offset: Int
     ){
-        //TODO implement me
+        spannableContent.getSpans<SearchFocusSpan>().forEach { spannableContent.removeSpan(it)}
+        spannableContent.setSpan(
+            SearchFocusSpan(),
+            searchPosition.first.minus(offset),
+            searchPosition.second.minus(offset),
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
     }
 
     fun clearSearchResult(){
-        //TODO implement me
+        spannableContent.getSpans<SearchSpan>().forEach { spannableContent.removeSpan(it) }
     }
 }
