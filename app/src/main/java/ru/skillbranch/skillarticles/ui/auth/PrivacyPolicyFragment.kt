d0e1c585091188644bf132c588e8235c2c00c4ed
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-08-13 00:24:00 +0500

    fix tree for testing

--- app/src/main/java/ru/skillbranch/skillarticles/ui/auth/PrivacyPolicyFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/auth/PrivacyPolicyFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import ru.skillbranch.skillarticles.R
 
-class PrivacyPoliceFragment : Fragment() {
+class PrivacyPolicyFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/auth/AuthViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/auth/AuthViewModel.kt
@@ -6,7 +6,7 @@ import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
 import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 import ru.skillbranch.skillarticles.viewmodels.base.NavigationCommand
 
-class AuthViewModel(handle: SavedStateHandle) : BaseViewModel<AuthState>(handle, AuthState()) {
+class AuthViewModel(handle: SavedStateHandle) : BaseViewModel<AuthState>(handle, AuthState()), IAuthViewModel {
     private val repository = RootRepository
 
     init {
@@ -15,7 +15,7 @@ class AuthViewModel(handle: SavedStateHandle) : BaseViewModel<AuthState>(handle,
         }
     }
 
-    fun handleLogin(login: String, pass: String, dest: Int?) {
+    override fun handleLogin(login: String, pass: String, dest: Int?) {
         repository.setAuth(true)
         navigate(NavigationCommand.FinishLogin(dest))
     }
