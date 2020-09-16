f296c2c5070b093180796f1360ab1996cee2bf6e
Ramozjikevic <bro.brobro@mail.ru>
2020-09-16 22:40:02 +0500

    add app class

---
+++ app/src/main/java/ru/skillbranch/skillarticles/App.kt
@@ -0,0 +1,26 @@
+package ru.skillbranch.skillarticles
+
+import android.app.Application
+import android.content.Context
+import com.facebook.stetho.Stetho
+
+class App: Application() {
+
+
+    companion object {
+        private var instance : App? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+}
