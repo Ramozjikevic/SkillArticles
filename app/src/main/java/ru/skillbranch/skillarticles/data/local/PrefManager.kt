d12ad5cc3e8e90d4e2abf09150135936f148eee9
Ramozjikevic <bro.brobro@mail.ru>
2020-09-19 22:07:24 +0500

    PrefManager.kt

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/PrefManager.kt
@@ -1,14 +1,32 @@
 package ru.skillbranch.skillarticles.data.local
 
-import android.content.Context
 import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.preference.PreferenceManager
+import ru.skillbranch.skillarticles.App
+import ru.skillbranch.skillarticles.data.models.AppSettings
 
-class PrefManager(context: Context) {
-    internal val preferences : SharedPreferences by lazy { PreferenceManager(context).sharedPreferences }
-    var counter = 0
+object PrefManager {
+    internal val preferences : SharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(App.applicationContext())
+    }
 
     fun clearAll(){
         preferences.edit().clear().apply()
+    }
+
+    fun getAppSettings() : LiveData<AppSettings> {
+        //TODO implement
+        return MutableLiveData(AppSettings())
+    }
+
+    fun isAuth() : MutableLiveData<Boolean> {
+        //TODO implement
+        return MutableLiveData(false)
+    }
+
+    fun setAuth(auth: Boolean): Unit {
+        //TODO implement
     }
 }
