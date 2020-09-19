c564575e623c279d4c1c40d2afb5689b5a81f126
Ramozjikevic <bro.brobro@mail.ru>
2020-09-19 21:43:38 +0500

    add ArticlePersonalInfosDao.kt.kt

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/dao/ArticlePersonalInfosDao.kt
@@ -0,0 +1,54 @@
+package ru.skillbranch.skillarticles.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import ru.skillbranch.skillarticles.data.local.entities.ArticlePersonalInfo
+
+@Dao
+interface ArticlePersonalInfosDao : BaseDao<ArticlePersonalInfo> {
+
+    @Transaction
+    fun upsert(list: List<ArticlePersonalInfo>){
+        insert(list)
+            .mapIndexed{index, recordResult -> if(recordResult == -1L) list[index] else null}
+            .filterNotNull()
+            .also { if(it.isNotEmpty()) update(it) }
+    }
+
+    @Query(
+        """
+        UPDATE article_personal_infos SET is_like = NOT is_like, update_at = CURRENT_TIMESTAMP
+        WHERE article_id =:articleId
+    """
+    )
+    fun toggleLike(articleId: String): Int
+
+    @Query(
+        """
+        UPDATE article_personal_infos SET is_bookmark = NOT is_like, update_at = CURRENT_TIMESTAMP
+        WHERE article_id =:articleId
+    """
+    )
+    fun toggleBookmark(articleId: String): Int
+
+    @Transaction
+    fun toggleBookMarkOrInsert(articleId: String) {
+        if (toggleBookmark(articleId) == 0) insert(
+            ArticlePersonalInfo(
+                articleId = articleId,
+                isBookmark = true
+            )
+        )
+    }
+
+    @Transaction
+    fun toggleLikeOrInsert(articleId: String){
+        if (toggleBookmark(articleId) == 0) insert(
+            ArticlePersonalInfo(
+                articleId = articleId,
+                isLike = true
+            )
+        )
+    }
+}
