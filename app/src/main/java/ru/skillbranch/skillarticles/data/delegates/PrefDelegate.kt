2b0d7df064bb18031c88ef69e2748cdb94719ea1
Ramozjikevic <bro.brobro@mail.ru>
2020-09-24 01:04:28 +0500

    for test

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -1,6 +1,9 @@
 package ru.skillbranch.skillarticles.data.delegates
 
+import android.content.SharedPreferences
+import androidx.lifecycle.LiveData
 import ru.skillbranch.skillarticles.data.local.PrefManager
+import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -46,4 +49,55 @@ class PrefDelegate<T>(private val defaultValue: T) {
         }
     }
 
+}
+
+class PrefLiveDelegate<T>(
+    private val fieldKey: String,
+    private val defaultValue: T,
+    private val preferences: SharedPreferences
+) : ReadOnlyProperty<Any?, LiveData<T>> {
+    private var storedValue: LiveData<T>? = null
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): LiveData<T> {
+        if (storedValue == null) {
+            storedValue = SharedPreferenceLiveData(preferences, fieldKey, defaultValue)
+        }
+        return storedValue!!
+    }
+}
+
+internal class SharedPreferenceLiveData<T>(
+    var sharedPrefs: SharedPreferences,
+    var key: String,
+    var defValue: T
+) : LiveData<T>() {
+    private val preferencesChangeListener =
+        SharedPreferences.OnSharedPreferenceChangeListener { _, shKey ->
+            if(shKey == key) {
+                value = readValue(defValue)
+            }
+        }
+
+    override fun onActive() {
+        super.onActive()
+        value = readValue(defValue)
+        sharedPrefs.registerOnSharedPreferenceChangeListener(preferencesChangeListener)
+    }
+
+    override fun onInactive() {
+        sharedPrefs.unregisterOnSharedPreferenceChangeListener(preferencesChangeListener)
+        super.onInactive()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun readValue(defaultValue: T): T {
+        return when (defaultValue) {
+            is Int -> sharedPrefs.getInt(key, defaultValue as Int) as T
+            is Long -> sharedPrefs.getLong(key, defaultValue as Long) as T
+            is Float -> sharedPrefs.getFloat(key, defaultValue as Float) as T
+            is String -> sharedPrefs.getString(key, defaultValue as String) as T
+            is Boolean -> sharedPrefs.getBoolean(key, defaultValue as Boolean) as T
+            else -> error("This type $defaultValue can not be stored into Preferences")
+        }
+    }
 }
